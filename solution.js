ff5759f8e4e94a67bb027a66490dac2797e8c1bf
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-12 11:49:14 -0500

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,7 @@
+Given a string s,
+reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+var reverseWords = function(s) {
+    return s.split(" ").map(word => word.split("").reverse().join("")).join(" "); 
+
+};
